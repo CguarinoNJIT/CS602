20bb9bfe0228bec534cab30a0e67546d44afa6c2
Chris Guarino <chris.francis.guarino@gmail.com>
2022-02-02 21:11:46 -0500

    HW1 working

--- Week_2/HW1/ComputerScience.java
+++ Week_2/HW1/ComputerScience.java
@@ -1,14 +1,32 @@
 public class ComputerScience{
 
     //Class Data
-    private int course_id, credits; 
-    private String name, semester;
+
+    int course_id, credits;
+    private String name, semester; 
+    
 
     //Constructor
-    public ComputerScience(int numcourse_id, int numcredits,String coursename, String whensemester){ 
+    public ComputerScience(String coursename, int numcourse_id, int numcredits, String whensemester){ 
         
+        name = coursename;
         course_id = numcourse_id; 
-        credits = numcredits; 
+        credits = numcredits;  
+        semester = whensemester; 
     }
-    
+
+    //Setter
+    public void setCourse(String coursename, int numcourse_id, int numcredits, String whensemester){
+       
+        name = coursename;
+        course_id = numcourse_id;  
+        credits = numcredits;  
+        semester = whensemester;
+    }
+
+    //Getter 
+    public String toString(){
+        return "| Course: " + name + " | ID: " + course_id + " | Credits: " + credits + " | Semester: " + semester + " |";  
+    } 
+
 }
--- Week_2/HW1/ComputerScienceTest.java
+++ Week_2/HW1/ComputerScienceTest.java
@@ -1,8 +1,9 @@
 public class ComputerScienceTest { 
 
     public static void main(String[] args){
-
-        
+        ComputerScience course1 = new ComputerScience("Advanced Machine Learning", 732, 3, "Summer"); 
+        ComputerScience course2 = new ComputerScience(coursename, numcourse_id, numcredits, whensemester); 
+        ComputerScience course3 = new ComputerScience(coursename, numcourse_id, numcredits, whensemester); 
     }
     
 }

15d855214a42d3ace946f8f10fc81eddaa363eb8
Chris Guarino <chris.francis.guarino@gmail.com>
2022-02-04 15:57:02 -0500

    HW Done_1

--- Week_2/HW1/ComputerScience.java
+++ Week_2/HW1/ComputerScience.java
@@ -1,7 +1,6 @@
 public class ComputerScience{
 
     //Class Data
-
     int course_id, credits;
     private String name, semester; 
     
@@ -15,14 +14,14 @@ public class ComputerScience{
         semester = whensemester; 
     }
 
-    //Setter
-    public void setCourse(String coursename, int numcourse_id, int numcredits, String whensemester){
+    // //Setter - Not Needed
+    // public void setCourse(String coursename, int numcourse_id, int numcredits, String whensemester){
        
-        name = coursename;
-        course_id = numcourse_id;  
-        credits = numcredits;  
-        semester = whensemester;
-    }
+    //     name = coursename;
+    //     course_id = numcourse_id;  
+    //     credits = numcredits;  
+    //     semester = whensemester;
+    // }
 
     //Getter 
     public String toString(){
--- Week_2/HW1/ComputerScienceTest.java
+++ Week_2/HW1/ComputerScienceTest.java
@@ -2,8 +2,12 @@ public class ComputerScienceTest {
 
     public static void main(String[] args){
         ComputerScience course1 = new ComputerScience("Advanced Machine Learning", 732, 3, "Summer"); 
-        ComputerScience course2 = new ComputerScience(coursename, numcourse_id, numcredits, whensemester); 
-        ComputerScience course3 = new ComputerScience(coursename, numcourse_id, numcredits, whensemester); 
+        ComputerScience course2 = new ComputerScience("Advanced Data Security and Privacy", 708, 3, "Spring"); 
+        ComputerScience course3 = new ComputerScience("Artifical Intelligence", 670, 3, "Spring"); 
+
+        System.out.println(course1); 
+        System.out.println(course2); 
+        System.out.println(course3);
     }
     
 }
